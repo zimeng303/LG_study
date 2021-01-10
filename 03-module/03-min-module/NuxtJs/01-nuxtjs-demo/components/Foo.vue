09fc07bd66368bedc068f73209804e6755f75ea7
zimeng303 <li_shengnan0303@163.com>
2021-01-10 22:28:41 +0800

    async-data

---
+++ 03-module/03-min-module/NuxtJs/01-nuxtjs-demo/components/Foo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>FooPage</h1>
+      <p>{{ foo }}</p>
+      <ul>
+          <li v-for="item in posts" :key="item.id">
+              {{ item.title }}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooPage',
+    props: ["posts"]
+    // async asyncData () {
+    //     return {
+    //         foo: 'bar'
+    //     }
+    // }
+}
+</script>
+
+<style>
+
+</style>
--- 03-module/03-min-module/NuxtJs/01-nuxtjs-demo/pages/index.vue
+++ 03-module/03-min-module/NuxtJs/01-nuxtjs-demo/pages/index.vue
@@ -1,12 +1,38 @@
 <template>
   <div id="app">
-      <h1>Hello Nuxt.js!</h1>
+      <h1>{{ title }}</h1>
+      <nuxt-link to="about">About</nuxt-link>
+      <br />
+      <foo :posts="posts" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Foo from '@/components/Foo'
+
 export default {
-    name: 'App'
+    name: 'HomePage',
+    components: {
+      Foo
+    },
+    // 当你想要动态页面内容，有利于 SEO 或者是 提升首屏渲染速度的时候，就在 asyncData 中发请求获取数据
+    async asyncData () {
+      console.log('asyncData');
+      console.log(this); // undefined
+      // 首屏渲染
+      const res = await axios({
+        method: 'GET',
+        url: 'http://localhost:3000/data.json'
+      })
+      return res.data
+    },
+    // 如果是非异步数据或者普通数据，则正常的初始化到 data 中即可
+    data () {
+      return {
+        foo: 'bar'
+      }
+    }
 }
 </script>
 
