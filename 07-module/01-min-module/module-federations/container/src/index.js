565081c2b46ae600b2fc0fc06048a3ee3fb2dc1c
lishnan <lishnan@yonyou.com>
2022-03-02 10:13:32 +0800

    微前端-模块联邦

---
+++ 07-module/01-min-module/module-federations/container/src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @description 容器应用的入口文件
+ */
+
+/* import("products/Index").then(products => {
+  console.log(products);
+}) */
+
+import('./bootstrap.js')
+
---
+++ 07-module/01-min-module/module-federations/products/src/index.js
@@ -0,0 +1,5 @@
+/**
+ * @description 应用的入口文件
+ */
+
+import("./bootstrap")
